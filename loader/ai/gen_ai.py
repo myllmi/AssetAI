965c713876ee82f3ee132236a84b64ba0039085e
Jorge Nascimento <jnascimento.gcc@gmail.com>
2025-05-23 14:55:45 +0100

    Add Embeding LLAMA3

--- loader/ai/gen_ai.py
+++ loader/ai/gen_ai.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from langchain_ollama import OllamaEmbeddings
 from langchain_openai import OpenAIEmbeddings
 from langchain_voyageai import VoyageAIEmbeddings
 
@@ -7,6 +8,7 @@ load_dotenv()
 # Const Embedding models
 EMBEDDING_OPENAI_TEXT_3_SMALL = 'OPENAI_TEXT_3_SMALL'
 EMBEDDING_VOYAGE_TEXT_3_SMALL = 'VOYAGE_TEXT_3_SMALL'
+EMBEDDING_META_LLAMA_3 = 'META_LLAMA_3'
 
 
 class AIEmbedding:
@@ -17,6 +19,8 @@ class AIEmbedding:
     def select_embedding_model(_embedding_model=EMBEDDING_OPENAI_TEXT_3_SMALL):
         if _embedding_model == EMBEDDING_VOYAGE_TEXT_3_SMALL:
             return VoyageAIEmbeddings(model='voyage-3.5', batch_size=1)
+        elif _embedding_model == EMBEDDING_META_LLAMA_3:
+            return OllamaEmbeddings(model="llama3")
         else:
             return OpenAIEmbeddings(model="text-embedding-3-small")
 
